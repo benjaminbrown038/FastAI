aff967666ed5cf5faafb32cb9385cbe14f87380e
Benjamin Brown <35114142+benjaminbrown038@users.noreply.github.com>
2023-07-04 00:58:17 -0600

    Update and rename fastai0.py to fastai.py while documenting and commenting code

--- scripts/fastai.py
+++ scripts/fastai.py
@@ -1,3 +1,9 @@
+
+'''
+
+'''
+
+
 from fastai.vision.all import *
 from fastai.callback.hook import *
 
@@ -6,15 +12,32 @@ path = untar_data(URLs.PETS)/'images'
 def is_cat(x):
     return x[0].isupper()
 
+'''
+
+'''
+
 dls = ImageDataLoaders.from_name_func(path,get_image_files(path),
                                         valid_pct = .2,seed=42,label_func = is_cat,
                                         item_tfms = Resize(224))
+
+'''
+
+'''
+
 learn = cnn_learner(dls,resnet34,metrics = error_rate)
 learn.fine_tune(1)
+
+'''
+
+'''
 
 path = untar_data(URLs.MNIST_SAMPLE)
 time = torch.arange(0,20)
 params = torch.randn(3).requires_grad_()
+
+'''
+
+'''
 
 def apply_step(params,prn=True):
     speed = time*3 + (time-9.5)**2 + 1
@@ -30,33 +53,61 @@ def apply_step(params,prn=True):
         print(loss.item())
         return pred
 
+'''
+
+'''
+
 def L1_loss(average,real):
     result = (average - real).abs().mean()
     return result
 
+'''
+
+'''
+
 def mean_sq_error_loss(average,real):
     result = ((average-real)**2).sqrt().mean()
     return result
+
+'''
+
+'''
 
 # weigths
 def init_params(size,std=1.0):
     params = (torch.randn(size)*std).requires_grad_()
     return params
 
+'''
+
+'''
+
 # train
 def linear1(xb):
     weights = xb@weights + bias
     return weights
+
+'''
+
+'''
 
 # activation
 def sigmoid(x):
     sig = 1/(1+torch.exp(-x))
     return sig
 
+'''
+
+'''
+
 # loss
 def mnist_loss(predictions,targets):
     predictions = predictions.sigmoid()
     return torch.where(targets==1,1-predictions,predictions).mean()
+
+'''
+
+'''
 
 # train
 def calc_grad(xb,yb,model):
@@ -64,6 +115,9 @@ def calc_grad(xb,yb,model):
     loss = mnist_loss(preds,yb)
     loss.backward()
 
+'''
+
+'''
 
 def batch_accuracy(xb,yb):
     preds = xb.sigmoid()
@@ -71,11 +125,19 @@ def batch_accuracy(xb,yb):
     result = correct.float().mean()
     return result
 
+'''
+
+'''
+
 # metrics
 def validate_epoch(model):
     accs = [batch_accuracy(model(xb),yb) for xb,yb in valid_dl]
     result = round(torch.stack(accs).mean().item(),4)
     return result
+
+'''
+
+'''
 
 # train
 def train_epoch(model,dl,opt):
@@ -84,11 +146,19 @@ def train_epoch(model,dl,opt):
         opt.step()
         opt.zero_grad()
 
+'''
+
+'''
+
 # train
 def train_model(model,epochs):
     for i in range(epochs):
         train_epoch(model)
         print(validate_epoch(model),end=' ')
+
+'''
+
+'''
 
 # train
 def simple_net(xb):
@@ -97,36 +167,64 @@ def simple_net(xb):
     res = res@w2 + b2
     return res
 
+'''
+
+'''
+
 # loading data
 def load_data(folder_name):
     training_tensor = [tensor(Image.open(i)) for i in folder_name]
     training_stack = ((torch.stack(training_tensor)).float())
     return training_stack
 
+'''
+
+'''
+
 # transforming data
 def training_data(*args):
     training = (torch.cat(args))
     return training
+
+'''
+
+'''
 
 # data information
 def size(training_stack):
     size = ((training_stack.shape)[1]) * (training_stack.shape[2])
     return size
 
+'''
+
+'''
+
 # creating data
 def init_weights(size):
     weights = (torch.randn(size)).requires_grad_()
     return weights
+
+'''
+
+'''
 
 # creating data
 def bias():
     bias = torch.randn(1)
     return bias
 
+'''
+
+'''
+
 # transforming data
 def transform_data_for_model(training_stack):
     result = training_stack[1] * training_stack[2]
     return result
+
+'''
+
+'''
 
 # transforming data
 def matrix_multiply(training_stack):
@@ -134,25 +232,45 @@ def matrix_multiply(training_stack):
     pred = ((new_training_stack) @ weights) + bias
     return pred
 
+'''
+
+'''
+
 # metric
 def loss(pred,target):
     loss = (pred-target).abs().mean()
     return loss
+
+'''
+
+'''
 
 # train
 def update(lr):
     new_weights -= weights.grad * lr
     return new_weights
 
+'''
+
+'''
+
 # data information
 def size_of_image(image):
     image_size = image.shape
     return image_size
 
+'''
+
+'''
+
 # data transformation
 def apply_kernel(row,col,kernel):
     convolution = (img[row-1:row+2,col-1:col+2] * kernel).sum()
     return convolution
+
+'''
+
+'''
 
 # transformation
 def convolution_top():
@@ -160,15 +278,27 @@ def convolution_top():
     top_edge = tensor([[apply_kernel(i,j,top_edge) for j in rng] for i in rng])
     return top_edge
 
+'''
+
+'''
+
 # information
 def row(padding, stride, height):
     new_row = (height + padding) // stride
     return new_row
 
+'''
+
+'''
+
 # information
 def column(padding,stride,height):
     new_column = (height + padding) // stride
     return new_column
+
+'''
+
+'''
 
 # information
 def output_shape(w,n,p,f):
@@ -176,30 +306,54 @@ def output_shape(w,n,p,f):
     new_output = (w - n + (2*p) - f) + 1
     return new_output
 
+'''
+
+'''
+
 # creating kernels
 def top_edge():
     top_edge = (tensor([1,1,1],[0,0,0],[-1,-1,-1])).float()
     return top_edge
+
+'''
+
+'''
 
 # creating kernels
 def bottom_edge():
     bottom_edge = (tensor([-1,-1,-1],[0,0,0],[1,1,1])).float()
     return bottom_edge
 
+'''
+
+'''
+
 # creating kernels
 def right_edge():
     right_edge = (tensor([-1,0,1],[-1,0,1],[-1,0,1])).float()
     return right_edge
+
+'''
+
+'''
 
 # creating kernels
 def left_edge():
     left_edge = (tensor([1,0,-1],[1,0,-1],[1,0,-1])).float()
     return left_edge
 
+'''
+
+'''
+
 # creating kernels
 def diag1_edge():
     diag1_edge = (tensor([1,0,-1],[0,1,0],[-1,0,1])).float()
     return diag1_edge
+
+'''
+
+'''
 
 class BasicOptim:
 
